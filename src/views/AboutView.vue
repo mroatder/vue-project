832fadb9954afd4a6994b8d94466a4eb2341cc93
mroatder <nattawat.six@gmail.com>
2024-10-27 18:10:18 +0700

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <div class="container">
+    <h1>This is an about page</h1>
+    <p>Nattawat Sommit</p>
+    <button type="button" class="btn btn-primary">Primary</button> &nbsp;
+    <button type="button" class="btn btn-secondary">Secondary</button>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+p{
+  color: rgb(224, 28, 28);
+}
+h1{
+  color: rgb(42, 29, 216);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- <div class="home">
+    <h1>Real Madrid FC</h1>
+    <img alt="Vue logo" src="../assets/Real_Madrid_CF.svg.png" height="200px" width="150px">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div class="carousel-inner">
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>Example headline.</h1>
+            <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Another example headline.</h1>
+            <p>Some representative placeholder content for the second slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item active">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>One more for good measure.</h1>
+            <p>Some representative placeholder content for the third slide of this carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+h1{
+  color: green;
+}
+</style>
+
+// <script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+// </script>
