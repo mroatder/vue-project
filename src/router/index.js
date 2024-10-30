b02a29781a00dec1a08f3ebeeaf6b141861714dc
mroatder <nattawat.six@gmail.com>
2024-10-30 13:59:24 +0700

    Update index.js
    Route: pricing page

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Pricing',
+    name: 'Pricing',
+
+    component: () => import('../views/Pricing.vue')
   }
 ]
 
