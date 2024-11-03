bf5f72773238571cf79bbf63dc64f0b017af68e0
mroatder <nattawat.six@gmail.com>
2024-11-03 14:27:08 +0700

    Create Product.vue

---
+++ src/views/Product.vue
@@ -0,0 +1,176 @@
+<template>
+   <div id="menu-container"></div>
+   
+   <div class="text-center mt-5">
+     <h1>Our Product</h1>
+     </div>
+   
+         <!-- show product -->
+ <div class="container text-center my-5 mx-auto">
+     <div class="row">
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2Frmcfmz0196-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Home Authentic Shirt 24/25 White" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens Home Authentic Shirt 24/25 White</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><a href="ProductDetail.html" target="_blank" rel="noopener noreferrer"><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></a></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMZ0204-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Third Authentic Shirt 24/25 Charcoal" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens Third Authentic Shirt 24/25 Charcoal</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMZ0201-1.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Away Authentic Shirt 24/25 Orange" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens Away Authentic Shirt 24/25 Orange</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMZ0197_01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens Goalkeeper Shirt 24/25 Blue</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFYO0056-01-1.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=640&q=75" 
+           alt="Youth adidas Urban Purist Tracksuit 24/25" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Youth adidas Urban Purist Tracksuit 24/25</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMS0320-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens adidas SZN Hoodie 24/25" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN Hoodie 24/25</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMS0318-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens adidas SZN Track Top 24/25" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN Track Top 24/25</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMS0321-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN Crew Sweatshirt 24/25 Orange</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMT0298-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN T-Shirt 24/25</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMT0296-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=640&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN Graphic T-Shirt 24/25 Orange</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMT0299-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Mens adidas SZN Graphic T-Shirt 24/25 Blue</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+       <div class="col-md-3 p-3">
+         <div class="overflow-hidden">
+           <img src="https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFLT0059-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522forceFilterByCatalogIncludeInheritance%2522%3Afalse%2C%2522forceFilterByCatalogExcludeInheritance%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=1920&q=75" 
+           alt="Mens Goalkeeper Shirt 24/25 Blue" width="200" height="200" class="img-fluid rounded">
+         </div>
+         <p>Womens adidas Oversized T-Shirt 24/25</p>
+         <del>$24.00 </del>&nbsp;
+         <span class="text-dark fw-semibold"> $18.00 </span>&nbsp;
+         <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary"> 10%
+           OFF</span>
+         <p><button id="btn1" class="btn btn-outline-success mt-2">add cart</button></p>
+       </div>
+     </div>
+     </div>
+</template>
+
+<style>
+        /* กำหนดการเคลื่อนไหวแบบนุ่มนวล */
+        .overflow-hidden img {
+          transition: transform 0.5s ease;
+        }
+        
+        /* เมื่อเลื่อนเมาส์ผ่าน */
+        .overflow-hidden:hover img {
+          transform: scale(1.1); /* ขยายรูปภาพเมื่อ hover */
+        }
+      </style>
+
+
+
+
+
+
+
